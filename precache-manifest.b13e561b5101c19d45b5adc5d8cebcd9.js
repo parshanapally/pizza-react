131a4fcac4811386b94931c3495d5ad2d1076d4a
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-26 14:25:36 -0600

    Updates

---
+++ precache-manifest.b13e561b5101c19d45b5adc5d8cebcd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pizza-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f56d410ea217d6cd0776",
+    "url": "/pizza-react/static/js/runtime~main.f56d410e.js"
+  },
+  {
+    "revision": "83febe43a35ddc09cba6",
+    "url": "/pizza-react/static/js/main.83febe43.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/pizza-react/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "83febe43a35ddc09cba6",
+    "url": "/pizza-react/static/css/main.d23548ce.chunk.css"
+  },
+  {
+    "revision": "1f8e1216bba7a55464b36bc7b2cb9cfa",
+    "url": "/pizza-react/index.html"
+  }
+];
